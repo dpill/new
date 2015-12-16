5ddf238be05ee0a9528b8c7c2013f034aaf410ec
dpill <a20141105059@mac05.local>
2015-12-16 19:11:05 +0800

    Initial Commit

---
+++ new/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  new
+//
+//  Created by 20141105059c on 15/12/16.
+//  Copyright © 2015年 20141105059c. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
